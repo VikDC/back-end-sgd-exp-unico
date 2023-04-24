1d569c35592eabfbcaced89a5359c741059d6ae5
Victor A. Del Castillo Pumahuallca <VDelCastillo@onpe.gob.pe>
2023-04-24 15:42:06 -0500

    sgd-exp-unico v1.2

---
+++ back-end-exp-unico/back-end-exp-unico/src/main/java/com/demo/modelo/LoginDTO.java
@@ -0,0 +1,27 @@
+package pe.gob.onpe.sarha.dto;
+
+public class LoginDTO{
+	String usuario;
+	String clave;
+	String captcha;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getCaptcha() {
+		return captcha;
+	}
+	public void setCaptcha(String captcha) {
+		this.captcha = captcha;
+	}
+	
+}
